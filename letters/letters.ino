7d0402195ac306fcd586a5379d18f7346a316499
allswellthatsmaxwell <allswellthatsmaxwell9@gmail.com>
2025-01-04 20:25:58 -0600

    hours working

--- letters/letters.ino
+++ letters/letters.ino
@@ -10,8 +10,8 @@
 
 CRGB leds[NUM_LEDS];
 
-const unsigned long INITIAL_HOURS = 19;
-const unsigned long INITIAL_MINUTES = 30;
+const unsigned long INITIAL_HOURS = 21;
+const unsigned long INITIAL_MINUTES = 4;
 String receivedData;
 String currentTime;
 String letters = "ITLISASTIMECDRETRAUQCATWENTYFIVEXOTFNETBFLAHPASTERUNINEEERHTXISENOFOURFIVETWONEVELETHGIESEVENTWELVEKCOLCOESNETPAMPIWINPMS";
@@ -20,8 +20,11 @@ const char* HOUR_WORDS[] = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN
 
 
 const int MAX_SIZE = 10;
-String targetWords[MAX_SIZE];
-int currentLength = 0;
+String targetWordsMinutes[MAX_SIZE];
+int targetWordsMinutesLength = 0;
+
+String targetWordsHours[MAX_SIZE];
+int targetWordsHoursLength = 0;
 
 void setup() {  
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
@@ -55,22 +58,43 @@ String readValue(String key, String data) {
 
 void setMinutesWords(int currentMinutes) {
   if (currentMinutes >= 23 && currentMinutes <= 37) {
-    targetWords[currentLength++] = "HALF";
-    targetWords[currentLength++] = "PAST";
-  } else if (currentMinutes >= 8 && currentMinutes <= 22) {
-    targetWords[currentLength++] = "QUARTER";
-    targetWords[currentLength++] = "PAST";
-  } else if (currentMinutes >= 37 && currentMinutes <= 52) {
-    targetWords[currentLength++] = "QUARTER";
-    targetWords[currentLength++] = "TO";
+    targetWordsMinutes[targetWordsMinutesLength++] = "HALF";
+    targetWordsMinutes[targetWordsMinutesLength++] = "PAST";
+  } else if (currentMinutes >= 13 && currentMinutes <= 23) {
+    targetWordsMinutes[targetWordsMinutesLength++] = "QUARTER";
+    targetWordsMinutes[targetWordsMinutesLength++] = "PAST";
+  } else if (currentMinutes >= 8 && currentMinutes <= 12) {
+    targetWordsMinutes[targetWordsMinutesLength++] = "TEN";
+    targetWordsMinutes[targetWordsMinutesLength++] = "PAST";
+  } else if (currentMinutes >= 3 && currentMinutes <= 7) {
+    targetWordsMinutes[targetWordsMinutesLength++] = "FIVE";
+    targetWordsMinutes[targetWordsMinutesLength++] = "PAST";
+  } else if (currentMinutes >= 37 && currentMinutes <= 47) {
+    targetWordsMinutes[targetWordsMinutesLength++] = "QUARTER";
+    targetWordsMinutes[targetWordsMinutesLength++] = "TO";
+  } else if (currentMinutes >= 48 && currentMinutes <= 52) {
+    targetWordsMinutes[targetWordsMinutesLength++] = "TEN";
+    targetWordsMinutes[targetWordsMinutesLength++] = "TO";
+  } else if (currentMinutes >= 53) {
+    targetWordsMinutes[targetWordsMinutesLength++] = "FIVE";
+    targetWordsMinutes[targetWordsMinutesLength++] = "TO";
+  } else {
+    targetWordsMinutes[targetWordsMinutesLength++] = "OCLOCK";
   }
+
 }
 
 void setHoursWords(int currentHours) {  
   if (currentHours == 0) {  // midnight
       currentHours = 12;
+      targetWordsHours[targetWordsHoursLength++] = "AM";
+  } else if (currentHours < 12) {
+    targetWordsHours[targetWordsHoursLength++] = "AM";
+  } else {
+    targetWordsHours[targetWordsHoursLength++] = "PM";
   }
-  targetWords[currentLength++] = HOUR_WORDS[currentHours - 1];
+
+  targetWordsHours[targetWordsHoursLength++] = HOUR_WORDS[currentHours % 12 - 1];
 }
 
 void setTargetWordsBasedOnTime() {
@@ -78,8 +102,7 @@ void setTargetWordsBasedOnTime() {
   getTime(millis(), currentHours, currentMinutes);
 
   setMinutesWords(currentMinutes);
-  // setHoursWords(currentHours);
-
+  setHoursWords(currentHours);
 }
 
 void lightWord(String word) {
@@ -101,12 +124,13 @@ String reverseString(String str) {
   return reversed;
 }
 
+
 void getTime(unsigned long milliseconds, int& hours, int& minutes) {  
   unsigned long totalMinutes = (milliseconds / 1000) / 60;
   
   totalMinutes += (INITIAL_HOURS * 60) + INITIAL_MINUTES;
 
-  hours = (totalMinutes / 60) % 24;    // Use modulo 24 to keep in 24-hour format
+  hours = (totalMinutes / 60) % 24;
   minutes = totalMinutes % 60;
 }
 
@@ -116,10 +140,15 @@ void loop() {
   // String targetWords[] = {"TW", "EN", "TY", "TIME", "IS"};
   setTargetWordsBasedOnTime();
   // String targetWords[] = {"TWENTY", "QUARTER"};
-  int size = currentLength;
-  for (int i = 0; i < size; i++) {
-    lightWord(targetWords[i]);
-    lightWord(reverseString(targetWords[i]));
+  for (int i = 0; i < targetWordsMinutesLength; i++) {
+    lightWord(targetWordsMinutes[i]);
+    lightWord(reverseString(targetWordsMinutes[i]));
   }
+  for (int i = 0; i < targetWordsHoursLength; i++) {
+    lightWord(targetWordsHours[i]);
+    lightWord(reverseString(targetWordsHours[i]));
+  }
+  targetWordsMinutesLength = 0;
+  targetWordsHoursLength = 0;
   FastLED.show();
 }
